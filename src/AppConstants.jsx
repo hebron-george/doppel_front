1bdb98c31c38f439b43a5e65839f600ae54d638a
Hebron George <hebrontgeorge@gmail.com>
2020-04-12 22:05:45 -0500

    Hello world response from backend

---
+++ src/AppConstants.jsx
@@ -0,0 +1,8 @@
+// let APIRoot = "http://localhost:8000"; // When running locally
+let APIRoot = "https://doppel-back.herokuapp.com";
+
+export default {
+  APIEndpoints: {
+    HELLOWORLD: APIRoot + "/hello-world"
+  },
+};
